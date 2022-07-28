d8ef8dbbc2c1a5908d13470e81985038157b2dea
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-07-28 11:48:30 +0200

    clean code

--- tests/Button.test.tsx
+++ tests/Button.test.tsx
@@ -1,16 +1,5 @@
-// import * as ReactDOM from 'react-dom'
-// import { Button } from '../src'
-
-// describe('Button', () => {
-//     it('renders without crashing', () => {
-//         const div = document.createElement('div')
-//         ReactDOM.render(<Button />, div)
-//         ReactDOM.unmountComponentAtNode(div)
-//     })
-// })
-
 import React from 'react'
-import { render, fireEvent, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Button } from '../src'
 
 describe('Button', async () => {
