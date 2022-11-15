b292046b2739f8c1810c6a7fa9feb8e13be57a35
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-15 11:35:45 +0100

    optimize context

--- src/components/ConfigProvider/ConfigProvider.tsx
+++ src/components/ConfigProvider/ConfigProvider.tsx
@@ -1,19 +1,20 @@
 import defu from 'defu'
-import React from 'react'
-import { DeepPartial } from 'ts-essentials'
-import { defaultConfig, Config } from '../..'
+import React, { useMemo } from 'react'
+import { defaultConfig, Config, ConfigOverride } from '../..'
 import { UnstyledUiConfigContext } from './context'
 
 type ConfigProviderProps = {
-    config?: DeepPartial<Config>
+    config?: ConfigOverride
     children: React.ReactNode
 }
 
 export function ConfigProvider({ config = {}, children }: ConfigProviderProps) {
     const mergedConfig = defu(config, defaultConfig) as Config
 
+    const memoizedValue = useMemo(() => mergedConfig, [config])
+
     return (
-        <UnstyledUiConfigContext.Provider value={mergedConfig}>
+        <UnstyledUiConfigContext.Provider value={memoizedValue}>
             {children}
         </UnstyledUiConfigContext.Provider>
     )
