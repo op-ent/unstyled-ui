53f0a274a45569db65e85ce7cd6816c36a632c19
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-23 22:01:15 +0100

    migrate to pnpm and update dependencies

--- src/utils/create-config.ts
+++ src/utils/create-config.ts
@@ -1,9 +1,11 @@
 import defu from 'defu'
-import type { DeepPartial } from 'ts-essentials'
-import type { Config } from '..'
+import type { ConfigOverride } from '..'
 
-export function createConfig(config: DeepPartial<Config>) {
-    function extendConfig(customConfig: DeepPartial<Config>) {
+export function createConfig(config: ConfigOverride): {
+    config: ConfigOverride
+    extendConfig: (customConfig: ConfigOverride) => ConfigOverride
+} {
+    function extendConfig(customConfig: ConfigOverride): ConfigOverride {
         return defu(customConfig, config)
     }
     return { config, extendConfig }
