a14874a2666428620700ec5c86d8097f31b68cd6
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-24 23:09:41 +0100

    docs: test

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -1,18 +1,7 @@
-import { defineTheme } from 'pinceau'
+import { defineTheme, palette } from 'pinceau'
 
 export default defineTheme({
     colors: {
-        primary: {
-            50: '#e5edff',
-            100: '#bccaf9',
-            200: '#92a7ef',
-            300: '#6684e5',
-            400: '#3c61dd',
-            500: '#2248c3',
-            600: '#193899',
-            700: '#10286e',
-            800: '#061845',
-            900: '#00081d',
-        },
+        primary: palette('#3e63dd'),
     },
 })
