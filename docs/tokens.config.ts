6e724d2ead032166a44f12b84e00b59ba30bce9f
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-11-24 23:06:08 +0100

    docs: update colors

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -1,7 +1,18 @@
-import { defineTheme, palette } from 'pinceau'
+import { defineTheme } from 'pinceau'
 
 export default defineTheme({
     colors: {
-        primary: palette('#3e63dd'),
+        primary: {
+            50: '#e5edff',
+            100: '#bccaf9',
+            200: '#92a7ef',
+            300: '#6684e5',
+            400: '#3c61dd',
+            500: '#2248c3',
+            600: '#193899',
+            700: '#10286e',
+            800: '#061845',
+            900: '#00081d',
+        },
     },
 })
